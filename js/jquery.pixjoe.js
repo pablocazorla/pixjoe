bb4fe3c0be3e6cf0531012fdc2de84e35bb39600
pablocazorla <pablo.david.cazorla@gmail.com>
2016-03-12 19:20:34 -0300

    ready

--- js/jquery.pixjoe.js
+++ js/jquery.pixjoe.js
@@ -6,45 +6,80 @@
 (function($) {
 	"use strict";
 
-	$('document').ready(function() {
+	var addedCanvas = false,
+		settings = {
+			backgroundColor: '#EEE',
+			borderColor: '#CCC',
+			borderWidth: 2,
+			fontSize: 14,
+			fontFamily: 'Arial',
+			fontWeight: 'bold',
+			color: '#AAA'
+		},
+		canvas, w, h,
+		addCanvas = function() {
+			var $container = $('<div/>')
+				.css({
+					'position': 'absolute',
+					'display': 'none',
+					'overflow': 'hidden',
+					'width': '1px',
+					'height': '1px'
+				})
+				.appendTo('body'),
+				$canvas = $('<canvas id="pixjoe-' + Math.round(Math.random() * 9999999) + '"/>').appendTo($container);
 
-		var $canvas = $('<canvas id="pixjoe" width="600" height="400"/>').appendTo('body');
+			canvas = $canvas[0];
+			addedCanvas = true;
+		},
+		formattedSize = function(a) {
+			return (typeof a == 'undefined') ? 150 : parseInt(a);
+		},
+		render = function(img) {
+			if (!addedCanvas) {
+				addCanvas();
+			}
 
-		var c = $canvas[0].getContext('2d');
+			var $img = $(img);
 
-		var render = function($img) {
+			w = formattedSize($img.attr('width'));
+			h = formattedSize($img.attr('height'));
 
-			var width = parseInt($img.attr('width')),
-				height = parseInt($img.attr('height')),
-
-				backgroundColor = '#CCC',
-				borderColor = '#555',
-
-				borderWidth = 3;
-
-			///////////////////////////////////////////////
-			c.fillStyle = backgroundColor;
-			c.strokeStyle = borderColor;
-			c.strokeWidth = borderWidth;
+			canvas.width = w;
+			canvas.height = h;
 
 
-			c.rect(0,0,width,height);
+			var c = canvas.getContext('2d');
+
+			c.lineCap = 'square';
+			c.lineJoin = 'miter';
+			c.textAlign = 'center';
+			c.fillStyle = settings.backgroundColor;
+			c.strokeStyle = settings.borderColor;
+			c.lineWidth = 2 * settings.borderWidth;
+			c.font = settings.fontWeight + ' ' + settings.fontSize + 'px ' + settings.fontFamily;
+
+
+
+			c.rect(0, 0, canvas.width, canvas.height);
 			c.fill();
 			c.stroke();
+			c.fillStyle = settings.color;
+			c.fillText(w + 'x' + h, .5 * w, .5 * (h + settings.fontSize * .7));
 
-
+			$img.attr('src', canvas.toDataURL());
 		};
 
+
+	$('document').ready(function() {
 		$('img:not(.pixjoe-ready)').each(function() {
 			if ($(this).attr('src') == 'pixjoe') {
-				render($(this));
+				render(this);
 			}
 		});
 
 
 
 	});
-
-
 
 })(jQuery);
